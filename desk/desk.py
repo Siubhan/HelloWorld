9b68ad18f8a63cec30c2e2ba3e99264c14fedea9
Siubhan <bdthestar@yahoo.co.uk>
2019-03-12 22:33:41 +0200

    Intro to new lib

---
+++ desk/desk.py
@@ -0,0 +1,13 @@
+import pyforms
+from pyforms import basewidget as bw
+from pyforms import controls as ct
+class Desk(bw.BaseWidget):
+    def __init__(self):
+        super(Desk, self).__init__('To-do desk')
+
+        # Definition of the forms fields
+        self._firstname = ct.ControlLabel('NameDay')
+
+
+if __name__=='__main__':
+    pyforms.start_app(Desk, geometry=(500, 200, 200, 200))
